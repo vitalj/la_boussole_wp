d9bedc6534c0309f6673b5ad9c699707267d3eaa
nelep90 <37117860+nelep90@users.noreply.github.com>
2018-07-24 19:38:44 +0200

    Update footer.php

---
+++ wp-content/themes/boussole/footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Karuna
+ */
+
+?>
+		<?php if ( is_active_sidebar( 'sidebar-5' ) ) : ?>
+			<div class="full-width-widget-area widget-area">
+				<?php dynamic_sidebar( 'sidebar-5' ); ?>
+			</div>
+		<?php endif; ?>
+	</div><!-- #content -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<?php get_template_part( 'components/footer/footer', 'widgets' ); ?>
+		
+	</footer>
+</div><!-- #page -->
+<?php wp_footer(); ?>
+<script src="../../wp-content/themes/boussole/photos.js"></script> 
+
+
+</body>
+</html>
